08078160ec6780e69d3a3a1d4b9d4dbfe3eadbf7
lubnaqadah <lubna.alqadah@gmail.com>
2019-02-22 21:39:06 -0800

    testing with deployment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route} from 'react-router-dom'
+//import { BrowserRouter, Route} from 'react-router-dom'
 import Speakersgram from './Components/Speakersgram/Speakersgram';
 
-class App extends Component {
- 
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Route exact path = "/" component = {Speakersgram} />
-        </div>
-      </BrowserRouter>
+//class App extends Component {
+// 
+//  render() {
+//    return (
+//      <BrowserRouter>
+//        <div>
+//          <Route exact path = "/" component = {Speakersgram} />
+//        </div>
+//      </BrowserRouter>
+//
+//    );
+//  }
+//}
+//
+//export default App;
 
-    );
-  }
+
+//import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+//
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Speakersgram />
+            </div>
+        );
+    }
 }
 
 export default App;
